2fae1764de534b5c494264829af0f5f6231b3245
justinbeharry <jsbeharr@umich.edu>
2018-02-18 20:13:46 -0500

    Modified file for aliases
    Removed some of the aliases and placed them in new
    .aliases file in home directory.

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -3,11 +3,6 @@ setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
 
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
-
 alias -- -='cd -'
 alias 1='cd -'
 alias 2='cd -2'
@@ -18,17 +13,3 @@ alias 6='cd -6'
 alias 7='cd -7'
 alias 8='cd -8'
 alias 9='cd -9'
-
-alias md='mkdir -p'
-alias rd=rmdir
-alias d='dirs -v | head -10'
-
-# List directory contents
-alias lsa='ls -lah'
-alias l='ls -lah'
-alias ll='ls -lh'
-alias la='ls -lAh'
-
-# Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
